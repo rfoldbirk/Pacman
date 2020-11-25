2b6f42eba7c112d9eae90ec9dad1ebe0c6450668
Rasmus Foldberg Birkebæk <rasmusfoldberg@gmail.com>
2020-11-25 09:35:22 +0100

    godot projekt

--- constants.py
+++
@@ -1,4 +0,0 @@
-class Constants:
-    def __init__(self):
-        self.old_possibleDirections = [ 'left', 'right', 'up', 'down' ]
-        self.possibleDirections = [ 'up', 'left', 'down', 'right' ]
